3c10d5d3145568cf6c71a51e0a35264d63ee1a7b
pablocelentano@gmail.com <pablocelentano@gmail.com@196fbd1e-dd2e-3260-68d0-3ad9787916b3>
2011-05-20 14:02:09 +0000

    real servlets

---
+++ src/main/java/servlets/MyRealm.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import DAO.UserDAO;
+import model.User;
+import org.securityfilter.realm.SimpleSecurityRealmBase;
+
+public class MyRealm extends SimpleSecurityRealmBase {
+
+    public boolean booleanAuthenticate(String username, String password) {
+        User user = null;
+        try{
+            user = UserDAO.retrieveUserbyNickName(username).get(0);
+        }catch(IndexOutOfBoundsException e){
+            return false;
+        }
+        if(user!=null){
+            if(user.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isUserInRole(String username, String role) {
+        return true;
+    }
+
+
+
+}
