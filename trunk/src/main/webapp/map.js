09236a6155b056efb6120b5b66e943339ab0caff
marycraig90@gmail.com <marycraig90@gmail.com@196fbd1e-dd2e-3260-68d0-3ad9787916b3>
2011-06-24 04:26:10 +0000

    marcar lugares en el mapa!

--- trunk/src/main/webapp/map.js
+++ trunk/src/main/webapp/map.js
@@ -34,6 +34,57 @@ var infowindow = new google.maps.InfoWindow({
             content: contentString
         });
 
+function createContent(name, address) {
+    var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="windowName"> Name</h1>' +
+            '<div id="bodyContent">' +
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the ' +
+            'Northern Territory, central Australia. It lies 335 km (208 mi) ' +
+            'south west of the nearest large town, Alice Springs; 450 km ' +
+            '(280 mi) by road. Kata Tjuta and Uluru are the two major ' +
+            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+            'Aboriginal people of the area. It has many springs, waterholes, ' +
+            'rock caves and ancient paintings. Uluru is listed as a World ' +
+            'Heritage Site.</p>' +
+            '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+            'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>' +
+            '</div>' +
+            '</div>';
+    return contentString;
+}
+function getPlaces() {
+    var json = getJson("http://" + window.location.host + "/place.do?action=PLACE_MARKERS");
+    for (var i = 0; i < json.length; i += 4) {
+        var infowindow = new google.maps.InfoWindow({
+                    content: createContent(json[i], json[i + 1])
+                });
+        var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(json[i + 2], json[i + 3]),
+                    animation: google.maps.Animation.DROP
+                });
+        marker.setMap(map);
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+}
+
+function getJson(url) {
+    XMLHttpRequestObject = new XMLHttpRequest();
+
+    XMLHttpRequestObject.open("POST", url, false);
+
+    XMLHttpRequestObject.send(null);
+
+    var JSONtext = (XMLHttpRequestObject.responseText);
+    var JSONObject = eval('(' + JSONtext + ')');
+    return JSONObject;
+}
+
 function loadMap() {
 
     var mapLayout = document.getElementById("example");
@@ -49,10 +100,10 @@ function loadMap() {
 
         if (position.coords.latitude) {
             pos = position;
-            map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-       $.ajax({
-   url: "/user.do?action=UPDATE_LOCATION&latitude="+pos.coords.latitude+"&longitude="+pos.coords.longitude
-});
+           map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+            $.ajax({
+                        url: "/user.do?action=UPDATE_LOCATION&latitude=" + pos.coords.latitude + "&longitude=" + pos.coords.longitude
+                    });
 
         } else {
             map.setCenter(-34, -30);
@@ -65,17 +116,14 @@ function loadMap() {
 
         marker.setMap(map);
 
-            google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
 
 
     });
 
-
-
-
-
+    getPlaces();
 }
 
 
